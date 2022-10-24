d9263b760b295c0a9349aab96b655ab3ea02f258
AhmedMoussa-png <82889833+AhmedMoussa-png@users.noreply.github.com>
2022-10-24 06:48:16 +0200

    Update Ahmed Al Sunbati_ChargedRod.py

--- Ahmed Al Sunbati_ChargedRod.py
+++ Ahmed Al Sunbati_ChargedRod.py
@@ -1,54 +1,42 @@
 from vpython import *
 #Web VPython 3.2
-sf = 0.002 # Scale factor for electric field arrow.
-oofpez = 9e9 # Electric constant.
-Qtot = 1e-9 # Total charge.
-L = 1.0 # Length of rod.
-N = 10.0 # Number of slices the rod will be made of.
-dy = L/N # Length of a slice.
+sf = 0.002 
+oofpez = 9e9 
+Qtot = 1e-9 
+L = 1.0 
+N = 10.0
+dy = L/N 
 
-# Create a list of slices as point charges.
-slices = [] # An empty list.
+
+slices = []
 i = 0
-y0 = -L/2 + dy/2 # Center of bottom slice.
+y0 = -L/2 + dy/2
 while i < N:
-    # Create a single point charge.
     a = sphere(pos=vector(0,y0+i*dy,0), radius=dy/2.0, color=color.red, q=Qtot/N)
-    # Add point charge to list of slices.
     slices.append(a)
-    i = i + 1 # Move to the next slice.
+    i = i + 1 
     
-    
-# Create a list of observation locations.
-observations = [] # An empty list.
+observations = []
 
-yobs_list = [-0.75,-0.5,-0.25,0.0,0.25,0.5,0.75] # Height of observation locations.
+yobs_list = [-0.75,-0.5,-0.25,0.0,0.25,0.5,0.75] 
 for yobs in yobs_list:
-    
-    R = 0.15 # Radius of circle of observations.
-    dtheta = pi/6.0 # Angle between observation locations.
+    R = 0.15
+    dtheta = pi/6.0
     theta = 0
     while (theta < 2.0*pi):
         a = vector(R*cos(theta), yobs, R*sin(theta))
         observations.append(a)
         theta = theta + dtheta
 
-# You can add to the list of observation locations here!
-
-
-
-# Loop over ALL observation locations to calculate E at each location.
 
 for r_obs in observations:
-    # Calculate E from each slice.
     E_net = vector(0,0,0)
     for s in slices:
-        r = r_obs - s.pos # Relative position vector.
-        rhat = r/mag(r) # Unit vector.
-        E = (oofpez * s.q / mag(r)**2) * rhat # Calculate E for this slice.
-        E_net = E_net + E # Add this E to the total E.
+        r = r_obs - s.pos 
+        rhat = r/mag(r)
+        E = (oofpez * s.q / mag(r)**2) * rhat 
+        E_net = E_net + E 
         
     arrow(pos=r_obs, axis=sf*E_net)
     
-# Print the most recent electric field vector for reference.
 print(E_net)
